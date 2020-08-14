1e7950933255e2b8e77da5bf02753de3a6f42f22
chriscorchado125 <chris.corchado125@gmail.com>
2020-08-14 17:52:18 -0400

    removed pageSetup

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -2,9 +2,6 @@ var Home = require('../models/home');
 var async = require('async');
 
 exports.index = function (req, res, next) {
-  const pageSetup = require('../public/js/pageSetup');
-  //const getCurrentPage = require('../public/js/getCurrentPage');
-
   Home.find({}, 'name description resume linkedin azure').exec(function (err, data) {
     if (err) return next(err);
 
@@ -12,7 +9,6 @@ exports.index = function (req, res, next) {
       title: 'Chris Corchado - Homepage',
       titlePage: 'About Me',
       data: data,
-      utility: { pageSetup },
     });
   });
 };
