376f6fa160740b445b1cb8bebc8dae23013920f2
chriscorchado125 <chris.corchado125@gmail.com>
2020-08-22 14:32:23 -0400

    updated dates for company and created video page for projects

--- controllers/companyController.js
+++ controllers/companyController.js
@@ -5,41 +5,25 @@ exports.index = function (req, res, next) {
   const getMonthYear = require('../public/js/getMonthYear');
   const highlightSearch = require('../public/js/highlightSearch');
 
-  if (!req.query.q) {
-    Company.find({}, 'name description screenshots job_title start_date end_date').exec(
-      function (err, data) {
-        if (err) return next(err);
+  let search = { $regex: new RegExp(req.query.q, 'i') };
 
-        res.render('company', {
-          title: 'Chris Corchado - History - Online Portfolio and Resume',
-          data: data,
-          count: data.length,
-          utility: {
-            getMonthYear,
-            highlightSearch,
-          },
-        });
-      }
-    );
-  } else {
-    let search = { $regex: new RegExp(req.query.q, 'i') };
-
-    Company.find(
+  let queryParams = {
+    $or: [
       {
-        $or: [
-          {
-            name: search,
-          },
-          {
-            description: search,
-          },
-          {
-            job_title: search,
-          },
-        ],
+        name: search,
       },
-      'name description screenshots job_title start_date end_date'
-    ).exec(function (err, data) {
+      {
+        description: search,
+      },
+      {
+        job_title: search,
+      },
+    ],
+  };
+
+  Company.find(queryParams, 'name description screenshots job_title start_date end_date')
+    .sort({ _id: 1, end_date: 1, name: 1, created: 1 })
+    .exec(function (err, data) {
       if (err) return next(err);
 
       res.render('company', {
@@ -53,5 +37,4 @@ exports.index = function (req, res, next) {
         },
       });
     });
-  }
 };
--- controllers/projectController.js
+++ controllers/projectController.js
@@ -4,47 +4,34 @@ var async = require('async');
 exports.index = function (req, res, next) {
   const highlightSearch = require('../public/js/highlightSearch');
 
-  if (!req.query.q) {
-    Project.find(
-      {},
-      'screenshots name description company_name videos technology project_date'
-    ).exec(function (err, data) {
-      if (err) return next(err);
+  let search = { $regex: new RegExp(req.query.q, 'i') };
 
-      res.render('project', {
-        title: 'Chris Corchado - Projects - Online Portfolio and Resume',
-        data: data,
-        count: data.length,
-        utility: {
-          highlightSearch,
-        },
-      });
-    });
-  } else {
-    let search = { $regex: new RegExp(req.query.q, 'i') };
-
-    Project.find(
+  let queryParams = {
+    $or: [
       {
-        $or: [
-          {
-            name: search,
-          },
-          {
-            description: search,
-          },
-          {
-            company_name: search,
-          },
-          {
-            technology: search,
-          },
-          {
-            screenshots: search,
-          },
-        ],
+        name: search,
       },
-      'screenshots name description company_name videos technology project_date'
-    ).exec(function (err, data) {
+      {
+        description: search,
+      },
+      {
+        company_name: search,
+      },
+      {
+        technology: search,
+      },
+      {
+        screenshots: search,
+      },
+    ],
+  };
+
+  Project.find(
+    queryParams,
+    'screenshots name description company_name videos technology project_date'
+  )
+    .sort({ _id: 1, project_date: 1, name: 1, created: 1 })
+    .exec(function (err, data) {
       if (err) return next(err);
 
       res.render('project', {
@@ -57,5 +44,4 @@ exports.index = function (req, res, next) {
         },
       });
     });
-  }
 };
