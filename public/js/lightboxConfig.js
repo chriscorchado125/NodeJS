d89fbd8186d7094eff76792d9be2f372ebd29dd6
chriscorchado125 <chris.corchado125@gmail.com>
2020-10-20 15:56:30 -0400

    fixed lighbox prev and next arrows for Mac

---
+++ public/js/lightboxConfig.js
@@ -0,0 +1,6 @@
+$("a.gallery").featherlightGallery({
+    previousIcon: "<img src='https://chriscorchado.com/lightbox/images/left-arrow.png' alt='Prev' />",
+    nextIcon: "<img src='https://chriscorchado.com/lightbox/images/right-arrow.png' alt='Next' />",
+    galleryFadeIn: 200,
+    galleryFadeOut: 300
+});
